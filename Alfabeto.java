706caac78ecef5526cbae1063e9709bacfe5ff5b
Alisson Neres <53406391+Alisson7Neres@users.noreply.github.com>
2021-05-23 17:31:08 -0300

    Create Alfabeto.java

---
+++ Alfabeto.java
@@ -0,0 +1,11 @@
+
+class Alfabeto{
+
+public static void main(String[] args){
+  
+    for(char[] alfabeto = 'a'; alfabeto <= 'z'; alfabeto++){ 
+  System.out.print(alfabeto + " ");
+   }
+  // saída no console(a b c d e f g h i j k l m n o p q r s t u v w x y z )
+  }
+}
